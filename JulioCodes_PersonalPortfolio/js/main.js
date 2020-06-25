1e162e9283eaf6ef5f80ab30c11e48121638a82e
danBamikiya <dudeawesome732@gmail.com>
2020-06-25 03:57:23 +0100

    I changed some styles and added a few more projects

--- JulioCodes_PersonalPortfolio/js/main.js
+++ JulioCodes_PersonalPortfolio/js/main.js
@@ -2,13 +2,13 @@ $(document).ready(function () {
 	$(".menu-toggler").on("click", function () {
 		$(this).toggleClass("open");
 		$(".top-nav").toggleClass("open");
-	});
+	}); /** this will open the menu overlay  by adding the class 'open' to it*/
 
 	$(".top-nav .nav-link").on("click", function () {
 		$(this).removeClass("open");
 		$(".top-nav").removeClass("open");
 		$(".menu-toggler").removeClass("open");
-	});
+	}); /** this will close the menu overlay after one of the nav links has been clicked */
 
 	$("nav a[href*='#']").on("click", function () {
 		$("html, body").animate(
@@ -17,7 +17,7 @@ $(document).ready(function () {
 			},
 			2000
 		);
-	});
+	}); /** this will set the vertical position of the scroll bar to the top coordinate of the clicked nav-link element - 40px, the animation duration is 2 secs */
 
 	$("#up").on("click", function () {
 		$("html, body").animate(
@@ -26,11 +26,11 @@ $(document).ready(function () {
 			},
 			2000
 		);
-	});
+	}); /** this will set the vertical position of the scroll bar to 0(px), the animation duration is 2 secs */
 
 	AOS.init({
 		easing: "ease",
 		duration: 1080,
 		// once: true; /** this will make the animation happen only once **/
-	});
+	}); /** this adds the aos library to the page so that the animations can be added by addindg the class names to the elements */
 });
